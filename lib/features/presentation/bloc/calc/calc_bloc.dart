bd19d0b52d4fd1cf6fc033dedf00d3b627ec2a5e
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-10 17:57:56 +0545

    feat: update CalcBloc to handle operations and store history locally

--- lib/features/presentation/bloc/calc/calc_bloc.dart
+++ lib/features/presentation/bloc/calc/calc_bloc.dart
@@ -1,30 +1,40 @@
-import 'package:calculator/features/domain/repositories/calculator_repo.dart';
-import 'package:calculator/features/presentation/bloc/calc/calc_event.dart';
-import 'package:calculator/features/presentation/bloc/calc/calc_state.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../../domain/repositories/calculator_repo.dart';
+import 'calc_event.dart';
+import 'calc_state.dart';
+import '../../../data/local_storage/calculator_local_storage.dart'; // import storage
 
 class CalcBloc extends Bloc<CalcEvent, CalcState> {
   final CalculatorRepo calculatorRepo = CalculatorRepo();
+  final CalculatorLocalStorage storage = CalculatorLocalStorage();
 
   CalcBloc() : super(CalcInitial()) {
-    on<AddEvent>((event, emit) {
-     final result = calculatorRepo.add(num1: event.num1, num2: event.num2);
+    on<AddEvent>((event, emit) async {
+      final result = calculatorRepo.add(num1: event.num1, num2: event.num2);
       emit(CalcResultState(result: result));
+
+      await storage.saveCalculation('${event.num1} + ${event.num2} = $result');
     });
 
-    on<SubtractEvent>((event, emit) {
-    final result = calculatorRepo.subtract(num1: event.num1, num2: event.num2);
+    on<SubtractEvent>((event, emit) async {
+      final result = calculatorRepo.subtract(num1: event.num1, num2: event.num2);
       emit(CalcResultState(result: result));
+
+      await storage.saveCalculation('${event.num1} - ${event.num2} = $result');
     });
 
-    on<MultiplyEvent>((event, emit) {
-       final result = calculatorRepo.multiply(num1: event.num1, num2: event.num2);
+    on<MultiplyEvent>((event, emit) async {
+      final result = calculatorRepo.multiply(num1: event.num1, num2: event.num2);
       emit(CalcResultState(result: result));
+
+      await storage.saveCalculation('${event.num1} × ${event.num2} = $result');
     });
 
-    on<DivideEvent>((event, emit) {
-       final result = calculatorRepo.divide(num1: event.num1, num2: event.num2);
+    on<DivideEvent>((event, emit) async {
+      final result = calculatorRepo.divide(num1: event.num1, num2: event.num2);
       emit(CalcResultState(result: result));
+
+      await storage.saveCalculation('${event.num1} ÷ ${event.num2} = $result');
     });
   }
 }
