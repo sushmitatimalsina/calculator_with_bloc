d7663a3a5a2b0c5ada3427ccf381f055c1b504ca
sushmitatimalsina <sushmitatimalsina602@gmail.com>
2025-09-10 15:18:13 +0545

    feat: create calculator UI and integrate with CalcBloc

--- lib/features/presentation/pages/calc_home_page.dart
+++ lib/features/presentation/pages/calc_home_page.dart
@@ -1,22 +1,20 @@
 import 'package:calculator/features/domain/repositories/calculator_repo.dart';
+import 'package:calculator/features/presentation/bloc/calc/calc_bloc.dart';
+import 'package:calculator/features/presentation/bloc/calc/calc_event.dart';
+import 'package:calculator/features/presentation/bloc/calc/calc_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class CalcHomePage extends StatefulWidget {
-  const CalcHomePage({super.key});
+class CalcHomePage extends StatelessWidget {
+  CalcHomePage({super.key});
 
-  @override
-  State<CalcHomePage> createState() => _CalcHomePageState();
-}
-
-class _CalcHomePageState extends State<CalcHomePage> {
   final TextEditingController _controllerFirstNum = TextEditingController();
   final TextEditingController _controllerSecondNum = TextEditingController();
-  num result = 0;
-
-  final calculatorRepo = CalculatorRepo();
 
   @override
   Widget build(BuildContext context) {
+    final calcBloc = BlocProvider.of<CalcBloc>(context);
+
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -29,35 +27,23 @@ class _CalcHomePageState extends State<CalcHomePage> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               TextField(
-                controller: _controllerFirstNum, 
+                controller: _controllerFirstNum,
                 decoration: const InputDecoration(
                   labelText: 'First Number',
                   hintText: 'Enter first number',
                 ),
-                style: const TextStyle(
-                  fontSize: 2,
-                  fontWeight: FontWeight.w500,
-                ),
                 keyboardType: TextInputType.number,
               ),
-
               const SizedBox(height: 10),
-
               TextField(
-                controller: _controllerSecondNum, 
+                controller: _controllerSecondNum,
                 decoration: const InputDecoration(
                   labelText: 'Second Number',
                   hintText: 'Enter second number',
                 ),
-                style: const TextStyle(
-                  fontSize: 2,
-                  fontWeight: FontWeight.w500,
-                ),
                 keyboardType: TextInputType.number,
               ),
               const SizedBox(height: 20),
-
-           
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
@@ -67,68 +53,67 @@ class _CalcHomePageState extends State<CalcHomePage> {
                           _controllerSecondNum.text.isNotEmpty) {
                         num num1 = num.parse(_controllerFirstNum.text);
                         num num2 = num.parse(_controllerSecondNum.text);
-                        setState(() {
-                          result = calculatorRepo.add(num1: num1, num2: num2);
-                        });
+                        calcBloc.add(AddEvent(num1: num1, num2: num2));
                       }
                     },
-                    child: const Text(
-                      '+',
-                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
+                    child: const Text('+', style: TextStyle(fontSize: 20)),
                   ),
-                  
                   ElevatedButton(
                     onPressed: () {
                       if (_controllerFirstNum.text.isNotEmpty &&
                           _controllerSecondNum.text.isNotEmpty) {
                         num num1 = num.parse(_controllerFirstNum.text);
                         num num2 = num.parse(_controllerSecondNum.text);
-                        setState(() {
-                          result = calculatorRepo.subtract(num1: num1, num2: num2);
-                        });
+                        calcBloc.add(SubtractEvent(num1: num1, num2: num2));
                       }
                     },
                     child: const Text('-', style: TextStyle(fontSize: 20)),
                   ),
-
                   ElevatedButton(
                     onPressed: () {
                       if (_controllerFirstNum.text.isNotEmpty &&
                           _controllerSecondNum.text.isNotEmpty) {
                         num num1 = num.parse(_controllerFirstNum.text);
                         num num2 = num.parse(_controllerSecondNum.text);
-                        setState(() {
-                          result = calculatorRepo.multiply(num1: num1, num2: num2);
-                        });
+                        calcBloc.add(MultiplyEvent(num1: num1, num2: num2));
                       }
                     },
                     child: const Text('*', style: TextStyle(fontSize: 20)),
                   ),
-
                   ElevatedButton(
                     onPressed: () {
                       if (_controllerFirstNum.text.isNotEmpty &&
                           _controllerSecondNum.text.isNotEmpty) {
                         num num1 = num.parse(_controllerFirstNum.text);
                         num num2 = num.parse(_controllerSecondNum.text);
-                        setState(() {
-                          result = calculatorRepo.divide(num1: num1, num2: num2);
-                        });
+                        calcBloc.add(DivideEvent(num1: num1, num2: num2));
                       }
                     },
                     child: const Text('/', style: TextStyle(fontSize: 20)),
                   ),
                 ],
               ),
-
               const SizedBox(height: 20),
-
-         
-              Text(
-                'Result: $result',
-                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              )
+              BlocBuilder
+              
+              <CalcBloc, CalcState>(
+                bloc: calcBloc,
+                builder: (context, state) {
+                  if (state is CalcResultState) {
+                    return Text(
+                      'Result: ${state.result}',
+                      style: const TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    );
+                  } else {
+                    return const Text(
+                      'No Result',
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    );
+                  }
+                },
+              ),
             ],
           ),
         ),
